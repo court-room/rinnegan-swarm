f1b90deb18a8eb19ae4965a8bcca2bf3bbf31188
onlinejudge95 <onlinejudge95>
2020-08-23 18:14:42 +0530

    Adds service worker

--- bootstrap.py
+++ bootstrap.py
@@ -72,6 +72,7 @@ if __name__ == "__main__":
             "rinnegan-database-network",
             "rinnegan-backend-network",
             "rinnegan-queue-network",
+            "rinnegan-streaming-network",
         ]
     )
     obj.manage_volumes(["rinnegan-database-volume", "rinnegan-queue-volume"])
