82f2dddd05f42f5a607d4603075224520a5dd7ce
onlinejudge95 <onlinejudge95>
2020-08-24 15:03:26 +0530

    Bumps version of postgres

--- cleanup.sh
+++ cleanup.sh
@@ -3,4 +3,6 @@
 docker stack rm rinnegan
 docker system prune --all --force --volumes
 docker network prune --force
-docker secret rm database-password database-user backend-secret-key backend-database-string backend-test-database-string
+docker secret rm database-password database-user
+docker secret rm backend-secret-key backend-database-string backend-test-database-string
+docker secret rm streaming-database-password streaming-database-user
