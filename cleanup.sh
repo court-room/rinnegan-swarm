c51dbcd8ecb73f02b3038a5baa77e77b20537260
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-08-24 15:12:26 +0530

    Bumps version of postgres (#14)
    * Bumps version of postgres
    
    * Fixes redis placement constraint
    
    Co-authored-by: onlinejudge95 <onlinejudge95>

--- cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stack rm rinnegan
-docker system prune --all --force --volumes
-docker network prune --force
-docker secret rm database-password database-user backend-secret-key backend-database-string backend-test-database-string
