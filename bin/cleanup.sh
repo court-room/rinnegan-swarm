239462fe2a4853b55542214fe1558a8e920197bc
onlinejudge95 <onlinejudge95>
2020-09-01 20:39:30 +0530

    Updates the secrets to be cleanup

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -5,3 +5,4 @@ docker system prune --all --force --volumes
 docker network prune --force
 docker secret rm database-password database-user
 docker secret rm backend-secret-key backend-redis-string backend-database-string backend-test-database-string
+docker secret rm backend-twitter-consumer-key backend-twitter-consumer-secret backend-cloud-vendor backend-aws-access-key-id backend-aws-secret-access-key backend-s3-bucket
