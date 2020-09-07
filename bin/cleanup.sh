026f2c3fc7ad6eca2d26f1607e116da996740649
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-09-07 14:46:38 +0530

    Adds mongo service (#20)
    * Adds env var
    
    * Adds secret to service
    
    * Adds monkeylearn config
    
    * Adds to production
    
    * Adds volume section
    
    * Fixes black issue

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -4,5 +4,17 @@ docker stack rm rinnegan
 docker system prune --all --force --volumes
 docker network prune --force
 docker secret rm database-password database-user
-docker secret rm backend-secret-key backend-redis-string backend-database-string backend-test-database-string
-docker secret rm backend-twitter-consumer-key backend-twitter-consumer-secret backend-cloud-vendor backend-aws-access-key-id backend-aws-secret-access-key backend-s3-bucket
+docker secret rm backend-secret-key
+docker secret rm backend-redis-string
+docker secret rm backend-database-string
+docker secret rm backend-test-database-string
+docker secret rm backend-twitter-consumer-key
+docker secret rm backend-twitter-consumer-secret
+docker secret rm backend-cloud-vendor
+docker secret rm backend-aws-access-key-id
+docker secret rm backend-aws-secret-access-key
+docker secret rm backend-s3-bucket
+docker secret rm backend-monkeylearn-api-token
+docker secret rm backend-monkeylearn-model-id
+docker secret rm streaming-database-user
+docker secret rm streaming-database-password
