1f5726bd34c6d8637da4ef719dc825a01f65e76f
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-09-11 00:31:54 +0530

    Integrates Mongo & Monkeylearn API (#21)
    * Removes secret file
    
    * Adds mongo uri as string
    
    * Adds post limit as env var
    
    * Updates cleanup script
    
    * Adds changes to prod
    
    * temp
    
    * Updates mongo startup script
    
    * Data stored in mongo db
    
    * Adds changes to prod file
    
    * Fixes prod uri secret for mongo
    
    * Adds config for prod

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -3,12 +3,14 @@
 docker stack rm rinnegan
 docker system prune --all --force --volumes
 docker network prune --force
-docker secret rm database-password database-user
+docker secret rm database-password
+docker secret rm database-user
 docker secret rm backend-secret-key
 docker secret rm backend-redis-string
 docker secret rm backend-database-string
 docker secret rm backend-test-database-string
 docker secret rm backend-twitter-consumer-key
+docker secret rm backend-twitter-consumer-secret
 docker secret rm backend-twitter-consumer-secret
 docker secret rm backend-cloud-vendor
 docker secret rm backend-aws-access-key-id
@@ -16,5 +18,7 @@ docker secret rm backend-aws-secret-access-key
 docker secret rm backend-s3-bucket
 docker secret rm backend-monkeylearn-api-token
 docker secret rm backend-monkeylearn-model-id
+docker secret rm backend-streaming-connection-uri
 docker secret rm streaming-database-user
-docker secret rm streaming-database-password
+docker secret rm backend-streaming-database
+docker secret rm backend-streaming-collection
