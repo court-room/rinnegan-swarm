12cd5fbbd8d86950711046f8db99e82d5a33dbdf
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-09-20 01:19:58 +0530

    Integration/new relic (#26)
    * Adds env vars for new relic
    
    * Updates cleanup script
    
    * Updates env in prod file
    
    * Adds sub domains for monitoring and backend

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -7,6 +7,7 @@ docker secret rm database-password
 docker secret rm database-user
 docker secret rm backend-secret-key
 docker secret rm backend-redis-string
+docker secret rm backend-redis-queue
 docker secret rm backend-database-string
 docker secret rm backend-test-database-string
 docker secret rm backend-twitter-consumer-key
@@ -22,6 +23,8 @@ docker secret rm backend-streaming-string
 docker secret rm backend-streaming-auth-source
 docker secret rm backend-streaming-database
 docker secret rm backend-streaming-collection
+docker secret rm backend-new-relic-license-key
+docker secret rm backend-new-relic-app-name
 docker secret rm streaming-database-root-user
 docker secret rm streaming-database-root-password
 docker secret rm streaming-database-user
